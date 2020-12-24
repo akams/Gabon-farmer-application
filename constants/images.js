33eeba789f98b089ed09c85d1d804f49609077aa
akams <e.ndjawe@gmail.com>
2020-12-24 11:17:22 +0100

    feat: add prefetch image

---
+++ constants/images.js
@@ -0,0 +1,22 @@
+export function preloadImages(urlOfImages) {
+  // an array of urls of images
+  const preFetchTasks = [];
+  urlOfImages.forEach((url) => {
+    preFetchTasks.push(Image.prefetch(url));
+  });
+
+  Promise.all(preFetchTasks).then((results) => {
+    try {
+      let downloadedAll = true;
+      results.forEach((result) => {
+        if (!result) {
+          // error occurred downloading a pic
+          downloadedAll = false;
+        }
+      });
+    } catch (e) {
+      console.log('error: preload image');
+      console.log(e);
+    }
+  });
+}
--- navigation/Routes.js
+++ navigation/Routes.js
@@ -7,6 +7,14 @@ import { AuthContext } from './AuthProvider';
 import RootStackScreen from './RootStackScreen';
 import DrawNavigateScreen from './DrawNavigateScreen';
 
+import { preloadImages } from '../constants/images';
+
+import meatImg from '../assets/home/meats.jpg';
+import fruitsImg from '../assets/home/fruits.jpg';
+import vegetablesImg from '../assets/home/vegetables.jpg';
+import rizImg from '../assets/home/riz.jpg';
+import seafoodsImg from '../assets/home/seafoods.jpg';
+
 const Routes = () => {
   const { user, setUser } = useContext(AuthContext);
   const [initializing, setInitializing] = useState(true);
@@ -18,10 +26,19 @@ const Routes = () => {
 
   useEffect(() => {
     const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    const urlOfImages = [];
+    urlOfImages.push(meatImg); // Replace it with your actual URL
+    urlOfImages.push(fruitsImg);
+    urlOfImages.push(vegetablesImg);
+    urlOfImages.push(rizImg);
+    urlOfImages.push(seafoodsImg);
     return () => {
       subscriber();
+      preloadImages(urlOfImages);
     };
   }, []);
+
+  console.log('initializing', initializing);
 
   if (initializing) return null;
 
